b056340f599fa05e28d819f11f9bb51dbe204d33
AR <angelinrajan20@gmail.com>
2023-06-10 12:51:20 -0400

    added codes to js and html

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,18 +1,30 @@
 var viewHighscore = document.querySelector(".highscore");
 var time = document.querySelector(".Time");
 var startButton = document.querySelector(".StartButton");
-var question1 = ("What contains true/false?");
-var option1 = ("Boolean", "Number", "String", "CSS");
+//var homePageTitle = document.getElementsByClassName('heading');
+var instruction = document.getElementById('inst');
+//var question1 = ("What contains true/false?");
+//var option1 = ("Boolean", "Number", "String", "CSS");
+var titleQ = document.getElementById('Q1');
+var opt = document.querySelectorAll('.options');
 var timercount;
 var timer;
+time.textContent = "5"
 
+window.onload = function() {
+    var listitem = document.getElementsByClassName("quiz");
+    listitem.style= 'none';
+};
 startButton.addEventListener("click", startQuiz);
 
+
 function startQuiz() {
+    startButton.style.display= 'none';
+    instruction.style.display= 'none';
 console.log("click event happening");
-
-
+timercount = 5;
 startTimer();
+showQ(questionNo1);
 }
 
 
@@ -27,8 +39,37 @@ function startTimer() {
         // Tests if time has run out
         if (timercount === 0) {
             console.log("GameOver!!")
+            time.textContent = "Game Over! 😮";
           // Clears interval
           clearInterval(timer);
         }
       }, 1000);
 }
+
+
+let questionNo1 = {
+    Q1: 'Which of these is a car?',
+    options: ['Rolex', 'PS5', 'Southwest', 'M4'],
+    correct: 3
+
+};
+
+function showQ(q) {
+titleQ.textContent = q.Q1;
+console.log(opt);
+opt.forEach(function(element, index) {
+    element.textContent = q.options[index];
+    element.addEventListener('click', function() {
+        if (q.correct == index) {
+            console.log('Correct Answer!');
+            console.log(index);
+          } else {
+            console.log('Wrong Answer!');
+            console.log(index);
+          }
+    });
+});
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Angelin_coding-quiz
+
+Outside Source used for reference:
+
+Tried to create element in js instead of html. https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+How to have multiple choice question display: https://gamedevacademy.org/javascript-quiz-tutorial/#Question_-_HTML_Web_Page
